9a9122f89d99188f7c8592bda97a55eadb2b01fc
Allen Lowe <lallenlowe@gmail.com>
2018-12-24 13:49:43 -0800

    trying to get dijkstra pathfinding working

--- src/systems/EnemyMovementSystem.js
+++ src/systems/EnemyMovementSystem.js
@@ -4,6 +4,32 @@ const EnemyMovementSystem = (entity) => {
     if (!entity.hasComponent('Moveable') || !entity.hasComponent('Enemy')) {
         return;
     }
+
+    const map = entity.getMap();
+
+    /* input callback informs about map structure */
+    const isPassable = function(x, y) {
+        if (x < 0 || y < 0) {
+            return false;
+        } else if (x >= map.getWidth() || y >= map.getHeight()) {
+            return false;
+        }
+        return !map.isTileSolid(x, y) || entity.hasComponent('PassThroughSolids')
+    }
+
+    /* prepare path to given coords */
+    const dijkstra = new ROT.Path.Dijkstra(...Lootr.getPlayer().getCoordinates(), isPassable);
+
+    const path = [];
+    /* compute from given coords #1 */
+    dijkstra.compute(...entity.getCoordinates(), function(x, y) {
+        path.push([x,y]);
+    });
     
-    MoveableSystem(entity, Math.round((Math.random()*2))-1, Math.round((Math.random()*2))-1);
+    if (path.length) {
+        MoveableSystem(entity, ...path.shift());
+    } else {
+        // no path to player
+        MoveableSystem(entity, Math.round((Math.random()*2))-1, Math.round((Math.random()*2))-1);
+    }
 }
