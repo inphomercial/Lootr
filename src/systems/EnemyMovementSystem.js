ba23e491480d30a3c13bad28e2145216d49d9113
lallenlowe <lallenlowe@gmail.com>
2018-12-23 19:41:52 -0800

    decouple map from display, camera follows player (#5)
    * decouple map from display, camera follows player
    
    * slightly better starting positions

--- src/systems/EnemyMovementSystem.js
+++ src/systems/EnemyMovementSystem.js
@@ -4,6 +4,8 @@ const EnemyMovementSystem = (entity) => {
     if (!entity.hasComponent('Moveable') || !entity.hasComponent('Enemy')) {
         return;
     }
+    
+    console.log(`moving towards ${Lootr.getPlayer().getCoordinates()}`);
 
     MoveableSystem(entity, Math.round((Math.random()*2))-1, Math.round((Math.random()*2))-1);
 }
