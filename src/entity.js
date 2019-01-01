43bc69bb793b89b761e195fdd24b0cf449094cd9
Allen Lowe <lallenlowe@gmail.com>
2018-12-31 18:45:19 -0800

    make enemies move towards the last place they saw the player, then randomly again

--- src/entity.js
+++ src/entity.js
@@ -11,12 +11,17 @@ class Entity extends DynamicGlyph {
 		this._speed = template.speed;
 		this._turns = 0;
 		this._map = null;
+		this._lastKnownPlayerCoords = [];
 		
 		for (const key of Object.keys(template.componentList)) {
 			const component = Lootr.EntityComponents[key](template.componentList[key]);
 
 			this.addObjComponent(component);
 		}
+	}
+
+	getLastKnownPlayerCoords() {
+		return this._lastKnownPlayerCoords;
 	}
 
 	getTurns() {
@@ -41,6 +46,14 @@ class Entity extends DynamicGlyph {
 
 	setMap( map ){
 		this._map = map;
+	}
+
+	setLastKnownPlayerCoords(x, y) {
+		this._lastKnownPlayerCoords = [x, y];
+	}
+
+	forgetLastKnownPlayerCoords() {
+		this._lastKnownPlayerCoords = [];
 	}
 
 	act() {
