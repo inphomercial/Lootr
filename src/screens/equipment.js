ef083746bdd4983273a3e6381d4afb8295de0a44
unknown <inphomercial@gmail.com>
2019-11-24 23:51:53 -0700

    fixing tabs

--- src/screens/equipment.js
+++ src/screens/equipment.js
@@ -1,15 +1,15 @@
 'use strict';
 
 Lootr.Screens.Equipment = {
-    _caption: "Equipment Screen",
+	_caption: "Equipment Screen",
 
-    enter: function( gameDisplay ) {
-        LoggerDebug("Entered Equipment Screen");
-        
-        this._gameDisplay = gameDisplay;
-    },
+	enter: function( gameDisplay ) {
+		LoggerDebug("Entered Equipment Screen");
+		
+		this._gameDisplay = gameDisplay;
+	},
 
-    renderGame: function () {
+	renderGame: function () {
 		let player = Lootr.getPlayer();
 
 		this._gameDisplay.drawText(1, 1, "Equipment Screen");
@@ -18,38 +18,38 @@ Lootr.Screens.Equipment = {
 		let startingPostition = 3;
 
 		Object.keys(equipment).forEach(slot => {
-            const slotText = getProperSlotName(slot);
-            const itemNameText = (equipment[slot] == '') ? '' : equipment[slot].getName();
+			const slotText = getProperSlotName(slot);
+			const itemNameText = (equipment[slot] == '') ? '' : equipment[slot].getName();
 
-            const fullText = `${slotText}: ${itemNameText}`;
-            
+			const fullText = `${slotText}: ${itemNameText}`;
+			
 			this._gameDisplay.drawText(1, startingPostition, fullText);
 			startingPostition++;
 		});
-    },
+	},
 
-    exit: function() {
-        LoggerDebug("Exited Equipment Screen");
-    },
+	exit: function() {
+		LoggerDebug("Exited Equipment Screen");
+	},
 
-    handleInput( inputType, inputData ) {
-        Lootr.switchScreen(Lootr.Screens.Play);
-    }
+	handleInput( inputType, inputData ) {
+		Lootr.switchScreen(Lootr.Screens.Play);
+	}
 }
 
 const getProperSlotName = slot => {
-    switch (slot) {
-        case 'head':
-            return "Head";            
-        case 'hand_1':
-            return "Left Hand";                
-        case 'hand_2':
-            return "Right Hand";
-        case 'body':
-            return "Body";
-        case 'feet':
-            return "Feet";                            
-        default:
-            return "unknown slot"            
-    }
+	switch (slot) {
+		case 'head':
+			return "Head";            
+		case 'hand_1':
+			return "Left Hand";                
+		case 'hand_2':
+			return "Right Hand";
+		case 'body':
+			return "Body";
+		case 'feet':
+			return "Feet";                            
+		default:
+			return "unknown slot"            
+	}
 }
