c721a6e5aa8a18173fb575fa2d7afc95efc20d18
unknown <inphomercial@gmail.com>
2019-11-25 23:19:05 -0700

    Start of spells

---
+++ src/templates/spells.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Lootr.Templates.Spells.Fire = {
+    template: 'Fire',
+    name: "Fire",
+	char: ".",
+	foreground: "red",
+    isSolid: false,
+    onCastText: "You cast a fire ball",
+    onFailCastText: "You fail to cast a fire ball",
+    cast(source, target) {
+        console.log(`Casting fire from ${source} to ${target}`, source, target);
+    }
+}
+
